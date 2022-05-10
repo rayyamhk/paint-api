50bbf9cc7937848dfa7990ba776e6f60bbd168d8
Ray Yam <ryamhkdev@gmail.com>
2022-05-11 03:06:21 +0800

    add zoom

--- paint.js
+++ paint.js
@@ -1,20 +1,24 @@
 class Paint {
+  #dpr;
   #width;
   #height;
   #bbox;
   #ctx;
-  #background;
   #cleanUp = () => {};
+
+  // background may be zoomed, so store an additional copy of the original background.
+  #background;
+  #backgroundCache;
 
   #painters = {};
   #checkpoints = [];
   #activeCheckpoint = -1;
 
-  // zoom
+  // zoom states
   #zoomingIn = false;
-  #zoomFactor = 1;
-  #zoomX;
-  #zoomY;
+  #zf = 1; // factor
+  #zx; // center x
+  #zy; // center y
 
   // initial mousedown position
   #x;
@@ -37,15 +41,17 @@ class Paint {
   #flipY = 1;
 
   constructor(canvas, width, height) {
-    const dpr = window && window.devicePixelRatio || 1;
-    this.#width = width;
-    this.#height = height;
+    this.#dpr = window && window.devicePixelRatio || 1;
     canvas.style.width = width + 'px';
     canvas.style.height = height + 'px';
-    canvas.width = width * dpr;
-    canvas.height = height * dpr;
+    canvas.width = width * this.#dpr;
+    canvas.height = height * this.#dpr;
+    this.#width = width;
+    this.#height = height;
+    this.#zx = width / 2;
+    this.#zy = height / 2;
     this.#ctx = canvas.getContext('2d');
-    this.#ctx.scale(dpr, dpr);
+    this.#ctx.scale(this.#dpr, this.#dpr);
     this.#initDOM();
   }
 
@@ -74,18 +80,21 @@ class Paint {
     this.#dispatch('clear');
   }
 
-  checkpoint() {
+  save() {
+    this.#cleanUp();
+    this.#hideBoundingBox();
+
     const canvas = this.#ctx.canvas.cloneNode();
     const ctx = canvas.getContext('2d');
     ctx.drawImage(this.#ctx.canvas, 0, 0);
     this.#checkpoints.push(canvas);
     this.#activeCheckpoint = this.#checkpoints.length - 1;
-    this.#hideBoundingBox();
     this.#history.reset();
+    this.#cleanUp = () => {};
     this.#dispatch('checkpoint', { checkpoint: this.#activeCheckpoint });
   }
 
-  loadCheckpoint(i) {
+  restore(i) {
     if (this.#checkpoints[i]) {
       this.#activeCheckpoint = i;
       this.#history.reset();
@@ -102,13 +111,13 @@ class Paint {
 
   background(type, content) {
     if (type === 'fill' || type === 'image') {
-      const ctx = this.#background.getContext('2d');
       if (type === 'fill') {
-        ctx.fillStyle = content;
-        ctx.fillRect(0, 0, this.#width, this.#height);
+        this.#background.fillStyle = content;
+        this.#background.fillRect(0, 0, this.#width, this.#height);
       } else {
-        ctx.drawImage(content, 0, 0, this.#width, this.#height);
+        this.#background.drawImage(content, 0, 0, this.#width, this.#height);
       }
+      this.#backgroundCache.drawImage(this.#background.canvas, 0, 0);
       this.#dispatch('background');
     }
   }
@@ -141,20 +150,46 @@ class Paint {
       e.preventDefault();
       e.stopPropagation();
       if (!this.#zoomingIn) {
+        // modify zoom states
         this.#zoomingIn = true;
-        this.#zoomX = Math.min(Math.max(e.offsetX, hw), this.#width - hw);
-        this.#zoomY = Math.min(Math.max(e.offsetY, hh), this.#height - hh);
-        this.#zoomFactor = factor;
+        this.#zf = factor;
+        this.#zx = Math.min(Math.max(e.offsetX, hw), this.#width - hw);
+        this.#zy = Math.min(Math.max(e.offsetY, hh), this.#height - hh);
+
+        // transform the canvas
+        this.#ctx.canvas.style.cursor = 'zoom-out';
+        this.#ctx.save();
+        this.#ctx.scale(this.#zf, this.#zf);
+        this.#ctx.translate(-this.#zx + hw, -this.#zy + hh);
         this.#repaint();
+
+        // transform the background
+        this.#background.save();
+        this.#background.clearRect(0, 0, this.#width, this.#height);
+        this.#background.scale(this.#zf, this.#zf);
+        this.#background.translate(-this.#zx + hw, -this.#zy + hh);
+        this.#background.drawImage(this.#backgroundCache.canvas, 0, 0);
       } else {
+        // reset zoom states
         this.#zoomingIn = false;
-        this.#zoomFactor = 1;
+        this.#zf = 1;
+        this.#zx = this.#width / 2;
+        this.#zy = this.#height / 2;
+
+        // reset canvas
+        this.#ctx.canvas.style.cursor = 'zoom-in';
+        this.#ctx.restore();
         this.#repaint();
+
+        // reset background
+        this.#background.restore();
+        this.#background.clearRect(0, 0, this.#width, this.#height);
+        this.#background.drawImage(this.#backgroundCache.canvas, 0, 0);
       }
     };
 
     this.#cleanUp();
-    this.#ctx.canvas.style.cursor = 'zoom-in';
+    this.#ctx.canvas.style.cursor = this.#zoomingIn ? 'zoom-out' : 'zoom-in';
     this.#ctx.canvas.addEventListener('mousemove', onMouseMove);
     this.#ctx.canvas.addEventListener('mousedown', onMouseDown);
     this.#hideBoundingBox();
@@ -162,6 +197,7 @@ class Paint {
       this.#ctx.canvas.style.cursor = 'default';
       this.#ctx.canvas.removeEventListener('mousemove', onMouseMove);
       this.#ctx.canvas.removeEventListener('mousedown', onMouseDown);
+      this.#repaint(); // remove the zoom box.
     }
   }
 
@@ -175,8 +211,9 @@ class Paint {
 
     return (width, height, options = {}) => {
       const bboxState = {
-        centerX: this.#width * 0.5,
-        centerY: this.#height * 0.5,
+        // always placed in the center of the canvas
+        centerX: this.#zoomingIn ? this.#zx : this.#width * 0.5,
+        centerY: this.#zoomingIn ? this.#zy : this.#height * 0.5,
         width,
         height,
         rotate: 0,
@@ -210,8 +247,9 @@ class Paint {
     return (options = {}) => {
       const onMouseDown = (e) => {
         e.preventDefault();
-        this.#x = e.offsetX;
-        this.#y = e.offsetY;
+        const [x, y] = this.#zoomCoordinateToActual([e.offsetX, e.offsetY]);
+        this.#x = x;
+        this.#y = y;
         this.#painting = true;
         this.#history.push({
           id: this.#getId(),
@@ -227,8 +265,9 @@ class Paint {
         window.requestAnimationFrame(() => this.#repaint());
         this.#dispatch('brushstart', { type, x: this.#x, y: this.#y });
       };
-      const onMouseMove = ({ offsetX: x2, offsetY: y2 }) => {
+      const onMouseMove = ({ offsetX, offsetY }) => {
         if (this.#painting) {
+          const [x2, y2] = this.#zoomCoordinateToActual([offsetX, offsetY]);
           const trajectory = this.#history.at(-1).state.trajectory;
           const [x1, y1] = last(trajectory).pt;
           const dist = distance(x1, y1, x2, y2);
@@ -428,18 +467,32 @@ class Paint {
     });
   }
 
-  #showBoundingBox({ centerX: cx, centerY: cy, width: w, height: h, rotate: r }) {
-    const _w = Math.abs(w);
-    const _h = Math.abs(h);
-
-    // minimize the number of reflow.
-    if (this.#bbox._w !== w || this.#bbox._h !== h) {
-      this.#bbox.style.width = _w + 'px';
-      this.#bbox.style.height = _h + 'px';
+  // cursor coordinates to actual coordinate
+  #zoomCoordinateToActual(xy) {
+    if (this.#zoomingIn) {
+      xy[0] = this.#zx + (xy[0] - this.#width / 2) / this.#zf;
+      xy[1] = this.#zy + (xy[1] - this.#height / 2) / this.#zf;
     }
+    return xy;
+  }
 
+  #actualCoordinateToZoom(xy) {
+    if (this.#zoomingIn) {
+      xy[0] = this.#width / 2 + (xy[0] - this.#zx) * this.#zf;
+      xy[1] = this.#height / 2 + (xy[1] - this.#zy) * this.#zf;
+    }
+    return xy;
+  }
+
+  #showBoundingBox({ centerX: cx, centerY: cy, width: w, height: h, rotate: r }) {
+    const _w = Math.abs(w) * this.#zf;
+    const _h = Math.abs(h) * this.#zf;
+    const [_x, _y] = this.#actualCoordinateToZoom([cx, cy]);
+
+    this.#bbox.style.width = _w + 'px';
+    this.#bbox.style.height = _h + 'px';
     this.#bbox.style.transform = `
-      translate(${cx - _w * 0.5}px, ${cy - _h * 0.5}px)
+      translate(${_x - _w * 0.5}px, ${_y - _h * 0.5}px)
       rotate(${r}rad)
     `;
 
@@ -458,6 +511,10 @@ class Paint {
   #initDOM() {
     this.#background = this.#ctx.canvas.cloneNode();
     this.#background.id = 'paint-background';
+    this.#background = this.#background.getContext('2d');
+    this.#background.scale(this.#dpr, this.#dpr);
+    this.#backgroundCache = this.#ctx.canvas.cloneNode().getContext('2d');
+    this.#backgroundCache.scale(this.#dpr, this.#dpr);
 
     const wrapper = createWrapper(this.#width, this.#height);
     const overlay = createOverlay();
@@ -470,7 +527,7 @@ class Paint {
     this.#bbox = bbox;
     this.#ctx.canvas.parentNode.replaceChild(wrapper, canvas);
     wrapper.appendChild(canvas);
-    wrapper.appendChild(this.#background)
+    wrapper.appendChild(this.#background.canvas)
     wrapper.appendChild(bbox);
     wrapper.appendChild(overlay);
 
@@ -502,8 +559,8 @@ class Paint {
       e.preventDefault();
       this.#rotating = true;
       this.#shouldAppendHistory = true;
-      this.#x = bbox._cx - Math.abs(bbox._w) * 0.5 + e.offsetX;
-      this.#y = bbox._cy - Math.abs(bbox._h) * 0.5 + e.offsetY;
+      this.#x = bbox._cx - Math.abs(bbox._w) * 0.5 + e.offsetX / this.#zf; // the x, y are w.r.t. bbox, simply rescale it.
+      this.#y = bbox._cy - Math.abs(bbox._h) * 0.5 + e.offsetY / this.#zf;
       overlay.style.zIndex = 9999;
       this.#dispatch('onrotatestart');
     });
@@ -522,10 +579,9 @@ class Paint {
         });
       }
 
-      const x = e.offsetX;
-      const y = e.offsetY;
-      const dx = e.movementX;
-      const dy = e.movementY;
+      const [x, y] = this.#zoomCoordinateToActual([e.offsetX, e.offsetY]);
+      const dx = e.movementX / this.#zf;
+      const dy = e.movementY / this.#zf;
       const state = this.#history.at(-1).state;
 
       if (this.#dragging) {
@@ -631,7 +687,6 @@ const canvas = document.querySelector('#painting-board');
 const paint = new Paint(canvas, 1200, 1200);
 
 const image = document.querySelector('#image');
-// paint.loadImage(image, 300, 300);
 paint.background('image', image);
 
 
@@ -639,12 +694,13 @@ document.querySelector('#redo-btn').addEventListener('click', () => paint.redo()
 document.querySelector('#undo-btn').addEventListener('click', () => paint.undo());
 document.querySelector('#clear-btn').addEventListener('click', () => paint.clear());
 document.querySelector('#zoom-btn').addEventListener('click', () => paint.zoom(2));
-document.querySelector('#checkpoint-btn').addEventListener('click', () => paint.checkpoint());
-document.querySelector('#load-checkpoint-btn-1').addEventListener('click', () => paint.loadCheckpoint(1));
-document.querySelector('#load-checkpoint-btn-2').addEventListener('click', () => paint.loadCheckpoint(2));
+document.querySelector('#checkpoint-btn').addEventListener('click', () => paint.save());
+document.querySelector('#load-checkpoint-btn-1').addEventListener('click', () => paint.restore(1));
+document.querySelector('#load-checkpoint-btn-2').addEventListener('click', () => paint.restore(2));
 document.querySelector('#ellipse-btn').addEventListener('click', () => paint.ellipse(200, 200));
 document.querySelector('#rectangle-btn').addEventListener('click', () => paint.rectangle(300, 150));
 document.querySelector('#triangle-btn').addEventListener('click', () => paint.triangle(300*1.118, 300));
 document.querySelector('#marker-btn').addEventListener('click', () => paint.marker({ size : 20 }));
 document.querySelector('#eraser-btn').addEventListener('click', () => paint.eraser({ size: 20 }));
+document.querySelector('#load-img-btn').addEventListener('click', () => paint.loadImage(image, 300, 300));
 document.querySelector('#line-btn').addEventListener('click', () => paint.line());
